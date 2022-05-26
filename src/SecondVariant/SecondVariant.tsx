ec03b9e5072d51a21dc758a6d413a15fde4507ec
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-05-26 09:49:20 +0300

    add start styles

--- src/SecondVariant/SecondVariant.tsx
+++ src/SecondVariant/SecondVariant.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Monitor } from './Monitor/Monitor';
 import {Settings} from "./Settings/Settings";
 import {Counter} from "./Counter/Counter";
+import s from './SecondVariant.module.css'
 
 type SecondVariantPropsType = {
     min: number
@@ -11,7 +12,7 @@ type SecondVariantPropsType = {
 
 export const SecondVariant: React.FC<SecondVariantPropsType> = ({}) => {
     return (
-        <div>
+        <div className={s.wrapper}>
             <Settings/>
             {/*<Monitor/>*/}
             <Counter/>
