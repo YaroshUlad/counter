b37ba86ff9799a701ed75b06c3c4302f641a9a09
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-05-26 10:24:02 +0300

    create ActionCreators for reducer

--- src/redux/secondVarReducer.ts
+++ src/redux/secondVarReducer.ts
@@ -3,9 +3,7 @@ type SecondStateType = {
     max: number,
     value: string
 }
-type ActionType = {
-    type: string
-}
+type ActionType = setMinAT | setMaxAT
 
 const initialState = {
     min: 0,
@@ -14,18 +12,34 @@ const initialState = {
 }
 
 const SET_MIN = 'SET_MIN'
-export const setMinAC = () => {
+export const setMinAC = (newMin: number) => {
     return {
-        type: SET_MIN
+        type: SET_MIN,
+        payload: {
+            newMin
+        }
     } as const
 }
+type setMinAT = ReturnType<typeof setMinAC>
+
+const SET_MAX = 'SET_MAX'
+export const setMaxAC = (newMax: number) => {
+    return {
+        type: SET_MAX,
+        payload: {
+            newMax
+        }
+    } as const
+}
+type setMaxAT = ReturnType<typeof setMaxAC>
 
 export const secondVarReducer = (state: SecondStateType = initialState, action: ActionType): SecondStateType => {
-    switch (action.type){
-        case '':
-            return state
+    switch (action.type) {
+        case SET_MIN:
+            return {...state, min: action.payload.newMin}
+        case SET_MAX:
+            return {...state, max: action.payload.newMax}
         default:
             return state
     }
-    return state
 }
