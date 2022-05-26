9005a8bedd2edd396a429522eea027abc3866e1b
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-05-26 10:30:56 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import {SecondVariant} from "./SecondVariant/SecondVariant";
 
 function App() {
-  const [min,setMin] = useState<number>(0)
-  const [max,setMax] = useState<number>(0)
-  const [value, setValue] = useState(min)
-  const increaseHandler = () => {
-    const newValue = value + 1
-    setValue(newValue)
-  }
-  const decreaseHandler = () => {
-      const newValue = value - 1
-      setValue(newValue)
-  }
-  const resetToMin = () => {
-      setValue(min)
-  }
+
   return (
     <div className="App">
-        {/*<div>{value}</div>
-        <button onClick={increaseHandler}>increase</button>
-        <button onClick={decreaseHandler}>decrease</button>
-        <button onClick={resetToMin}>reset</button>
-        <button>set</button>*/}
         <SecondVariant />
     </div>
   );
