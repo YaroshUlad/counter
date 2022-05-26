12e4cac7c077ba07d961ebc4d48343ac475b255e
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-05-26 11:05:13 +0300

    remove unused imports

--- src/SecondVariant/SecondVariant.tsx
+++ src/SecondVariant/SecondVariant.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Settings} from "./Settings/Settings";
 import {Counter} from "./Counter/Counter";
 import s from './SecondVariant.module.css'
 import {SettingsContainer} from "./Settings/SettingsContainer";
@@ -10,7 +9,6 @@ type SecondVariantPropsType = {
 export const SecondVariant: React.FC<SecondVariantPropsType> = () => {
     return (
         <div className={s.wrapper}>
-            {/*<Settings/>*/}
             <SettingsContainer/>
             <Counter/>
         </div>
