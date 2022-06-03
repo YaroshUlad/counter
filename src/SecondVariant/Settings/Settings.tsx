c384b755b7488d60c0e092341deb6bd97084c672
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-06-03 17:42:53 +0300

    refactor after delete SettingsContainer in Settings.tsx, delete all unused props

--- src/SecondVariant/Settings/Settings.tsx
+++ src/SecondVariant/Settings/Settings.tsx
@@ -3,16 +3,8 @@ import s from './Settings.module.css'
 import {Button} from "../../components/Button";
 import {useDispatch, useSelector} from "react-redux";
 import {getMax, getMin, getValue, setMaxAC, setMinAC, setSettingsAC} from "../../redux/secondVarReducer";
-/*
-type SettingsPropsType = {
-    value: string
-    min: string
-    max: string
-    setMin: (newMin: string) => void
-    setMax: (newMax: string) => void
-    saveSettings: () => void
-}*/
-export const Settings = (/*props: SettingsPropsType*/) => {
+
+export const Settings = () => {
     const min = useSelector(getMin)
     const max = useSelector(getMax)
     const value = useSelector(getValue)
@@ -22,16 +14,13 @@ export const Settings = (/*props: SettingsPropsType*/) => {
 
     const onMaxChange = (e: ChangeEvent<HTMLInputElement>) => {
         const newMax = e.currentTarget.value
-        //props.setMax(newMax)
         dispatch(setMaxAC(newMax))
     }
     const onMinChange = (e: ChangeEvent<HTMLInputElement>) => {
         const newMin = e.currentTarget.value
-        //props.setMin(newMin)
         dispatch(setMinAC(newMin))
     }
     const setSettings = () => {
-        //props.saveSettings()
         dispatch(setSettingsAC())
     }
     return (
