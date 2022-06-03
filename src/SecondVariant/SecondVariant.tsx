f5ec5f95db974085e373766751abb097b9b993c7
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-06-03 17:41:19 +0300

    refactor after delete SettingsContainer

--- src/SecondVariant/SecondVariant.tsx
+++ src/SecondVariant/SecondVariant.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import s from './SecondVariant.module.css'
-import {SettingsContainer} from "./Settings/SettingsContainer";
 import {CounterContainer} from "./Counter/CounterContainer";
 import {Settings} from "./Settings/Settings";
 
@@ -10,7 +9,6 @@ type SecondVariantPropsType = {
 export const SecondVariant: React.FC<SecondVariantPropsType> = () => {
     return (
         <div className={s.wrapper}>
-            {/*<SettingsContainer/>*/}
             <Settings/>
             <CounterContainer/>
         </div>
